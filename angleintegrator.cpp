f90a9687cdac00d13b6d9ad628eafef05fb6e772
Aerosol-man <65870273+Aerosol-man@users.noreply.github.com>
2023-07-17 23:58:27 +0100

    Minor algotithm optimisations

--- angleintegrator.cpp
+++ angleintegrator.cpp
@@ -8,9 +8,11 @@ AngleIntegrator::AngleIntegrator(QObject *parent, PlanetData *planetData)
     data = planetData;
 }
 
+//$ vectorise.m
 static auto angleFunction = xt::vectorize([](double ecc, double angle) -> double {
     return qPow(1 - ecc * qCos(angle), -2);
 });
+//$
 
 //$ interpolate.m
 xt::xtensor<double, 1> AngleIntegrator::interpolate(xt::xtensor<double, 1> &x, xt::xtensor<double, 1> &y, xt::xtensor<double, 1> &samplePoints)
@@ -21,43 +23,44 @@ xt::xtensor<double, 1> AngleIntegrator::interpolate(xt::xtensor<double, 1> &x, x
     {
         // Get the first number greater than the sample value
         auto upperBound = x.begin();
-
         while (*upperBound < samplePoints[i] && upperBound != x.end())
         {
             upperBound++;
         }
+
         if (upperBound == x.end())
         {
             out[i] = *upperBound;
-            continue;
         }
-
-        if (*upperBound != x[0])
+        else if (upperBound == x.begin())
+        {
+            out[i] = *upperBound;
+        }
+        else
         {
             // Get the last number less than the sample value
             auto lowerBound = upperBound; lowerBound--;
             // Get floating-point index of the value
-            double index = std::distance(x.begin(), lowerBound) + (samplePoints[i] - *lowerBound) / (*upperBound - *lowerBound);
-            out[i] = y[qFloor(index)] + (y[qCeil(index)] - y[ qFloor(index)]) * (index - qFloor(index));
+            double index = std::distance(x.begin(), lowerBound);
+            double remainder = (samplePoints[i] - *lowerBound) / (*upperBound - *lowerBound);
+            //linearly interpolate between the lower and upper bound based on the remainder
+            out[i] = y[index] + (y[qCeil(index + remainder)] - y[index]) * (remainder);
         }
-        else
-        {
-            out[i] = *upperBound;
-        }
+
     }
     return out;
 }
 //$ interpolate.m
 
+//$ simpson.m
 xt::xarray<double> AngleIntegrator::integrate(double period, double ecc, int periods, bool simplify, int n)
 {
-    double lastOrbitPeriod = 0.0;
     xt::xtensor<double, 1>::shape_type shape = {n * periods};
     auto interpolatedTheta = xt::xtensor<double, 1>::from_shape(shape);
     auto t = xt::xtensor<double, 1>::from_shape(shape);
 
-    double start = 0;
-    xt::xtensor<double, 1> theta = xt::arange(start, start + TAU, sampleSize);
+    xt::xtensor<double, 1> theta = xt::arange(0, TAU, sampleSize);
+    //calculate the integrand for each value of theta
     xt::xtensor<double, 1> integral = angleFunction(ecc, theta);
 
     // get all the even-numbered values (excluding the first & last value)
@@ -69,22 +72,19 @@ xt::xarray<double> AngleIntegrator::integrate(double period, double ecc, int per
 
     // Calculate the cumulative sum to get the sum at each t
     integral = xt::cumsum(integral);
-    integral *= sampleSize / 3;
 
-    xt::xtensor<double, 1> calculatedTime = period * qPow(1 - SQUARED(ecc), 1.5) * 1 / TAU * integral;
+    //Calculate the value of t for each value of theta
+    xt::xtensor<double, 1> calculatedTime = period * qPow(1 - SQUARED(ecc), 1.5) * 1 / (TAU * 3) * sampleSize * integral;
+    //Generate n evenly spaced times to sample theta from
     xt::xtensor<double, 1> _t = xt::linspace(xt::amin(calculatedTime)(), xt::amax(calculatedTime)(), n);
     xt::xtensor<double, 1> _interpolatedTheta = interpolate(calculatedTime, theta, _t);
-
+//$
     if (periods > 1)
     {
-        xt::view(t, xt::range(0, n)) = _t;
-        xt::view(interpolatedTheta, xt::range(0, n)) = _interpolatedTheta;
-
-        for (int i = 1; i < periods; i++)
-        {
-            xt::view(t, xt::range(i * n, (i + 1) * n)) = xt::view(t, xt::range(0, n)) + period * i;
-            xt::view(interpolatedTheta, xt::range(i * n, (i + 1) * n)) = xt::view(interpolatedTheta, xt::range(0, n)) + TAU * i;
-        }
+        t = xt::repeat(xt::arange<double>(0, periods) * period, periods, 0);
+        interpolatedTheta = xt::repeat(xt::arange<double>(0, periods) * TAU, periods, 0);
+        t += xt::tile(_t, periods);
+        interpolatedTheta += xt::tile(_interpolatedTheta, periods);
     }
     else {
         t = _t;
@@ -104,7 +104,6 @@ xt::xarray<double> AngleIntegrator::integrate(double period, double ecc, int per
     return out;
 }
 
-//$ simpson.m
 QVector<QVector2D> AngleIntegrator::fromValues(double period, double ecc, int periods, bool simplify)
 {
     QVector<QVector2D> out;
@@ -117,7 +116,6 @@ QVector<QVector2D> AngleIntegrator::fromValues(double period, double ecc, int pe
 
     return out;
 }
-//$ simpson.m
 
 QVector<QVector2D> AngleIntegrator::fromPlanet(int index, int periods, bool simplify)
 {
